5b995f4d93693a5fc41a7372ca8e3a3f8afcdb0e
Marshall-Diffey <marshalldiffey@yahoo.com>
2021-05-12 17:03:08 -0500

    Make login view

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    employee_number = StringField('Employee number', validators=[DataRequired])
-    password = PasswordField('Password', validators=[DataRequired])
+    employee_number = StringField('Employee number', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
